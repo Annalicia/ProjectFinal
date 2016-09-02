bdcf6b133310600b45cbab47ace67653e8d6c50f
Annalicia Ostos <annalicia@intagono.com>
2016-09-01 22:37:12 -0500

    Connection Push Notification

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.1'
+use_frameworks!
+
+target 'ProjectFinal' do
+pod 'Pushwoosh'
+pod 'Fabric'
+pod 'Digits'
+end
--- ProjectFinal/HomeController.h
+++ ProjectFinal/HomeController.h
@@ -9,7 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface HomeController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *btnAddPay;
 
+- (IBAction)addPay:(id)sender;
 
 @end
 
