556947bb5609b2f10b972d4d3dc0f3fa7582d50d
Annalicia Ostos <annalicia@intagono.com>
2016-09-01 19:12:44 -0500

    Initial Commit

---
+++ ProjectFinal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectFinal
+//
+//  Created by Annalicia Ostos on 01/09/16.
+//  Copyright © 2016 Codelko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProjectFinal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProjectFinal
+//
+//  Created by Annalicia Ostos on 01/09/16.
+//  Copyright © 2016 Codelko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
